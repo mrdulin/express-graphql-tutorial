41e0c68da1042ca0303a2a0f6574d8062a0f5240
mrdulin <novaline@qq.com>
2018-05-17 11:34:06 +0800

    add unit tests

--- src/mutation-with-input-types/server.spec.js
+++ src/mutation-with-input-types/server.spec.js
@@ -14,7 +14,7 @@ after(done => {
 });
 
 describe('mutation with input types test suites', () => {
-  it('t-0', () => {
+  it('should do the mutation correctly with operationName and variables', () => {
     const body = {
       query: `
         mutation createMessageForTest($input: MessageInput) {
@@ -31,6 +31,48 @@ describe('mutation with input types test suites', () => {
           content: 'I like her so much'
         }
       }
+    };
+
+    return rp(body).then(res => {
+      expect(res.data).to.have.property('createMessage');
+    });
+  });
+
+  it('should do the mutation correctly without operationName and variables', () => {
+    const body = {
+      query: `
+        mutation {
+          createMessage(input: {author: "hhh", content: "I love her"}) {
+            id
+            content
+            author
+          }
+        }
+      `
+    };
+
+    return rp(body).then(res => {
+      expect(res.data).to.have.property('createMessage');
+    });
+  });
+
+  // mutation中, JSON.stringify() input type类型的数据，graphql会报语法错误
+  it('t-3', () => {
+    const messageInput = {
+      author: 'hhh',
+      content: 'I like her so much'
+    };
+
+    const body = {
+      query: `
+        mutation {
+          createMessage(input: ${JSON.stringify(messageInput)}) {
+            id
+            content
+            author
+          }
+        }
+      `
     };
 
     return rp(body).then(res => {
